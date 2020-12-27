64d2acdc84a21ae04cb83437f15809bb20a80c70
Dr. Yad Tahir <yad@ieee.org>
2020-12-27 15:05:58 +0300

    FIX a crash bug when recycling through layouts

--- dwm.c
+++ dwm.c
@@ -761,12 +761,12 @@ cyclelayout(const Arg *arg)
 	Layout *l;
 	for (l = (Layout *)layouts; l != selmon->lt[selmon->sellt]; l++);
 	if (arg->i > 0) {
-		if (l->symbol && (l + 1)->symbol)
+		if (l != NULL && l->symbol && (l + 1)->symbol)
 			setlayout(&((Arg) { .v = (l + 1) }));
 		else
 			setlayout(&((Arg) { .v = layouts }));
 	} else {
-		if (l != layouts && (l - 1)->symbol)
+		if (l != NULL && l != layouts && (l - 1)->symbol)
 			setlayout(&((Arg) { .v = (l - 1) }));
 		else
 			setlayout(&((Arg) { .v = &layouts[LENGTH(layouts) - 2] }));
